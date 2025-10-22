967f8a5ea5538fca41191fdee2fa7bdc07963f79
Rinad Hazaea <rinadhazaea@gmail.com>
2025-10-22 20:58:07 +0200

    Import libraries for gender classification
    Added necessary imports for data processing and model training.

--- gender.py
+++ gender.py
@@ -1,3 +1,7 @@
 import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.neighbors import KNeighborsClassifier
 
+data = pd.read_csv("gender_by_name.csv")
 
