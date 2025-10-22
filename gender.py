dce90cf5d975fa3c0c89c40613ac4e1d5534f3f1
rafahbalut03-max <rafahbalut03@gmail.com>
2025-10-22 21:06:06 +0200

    Add KNeighborsClassifier and metrics imports

--- gender.py
+++ gender.py
@@ -1,5 +1,10 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 data = pd.read_csv("gender_by_name.csv")
+
 
